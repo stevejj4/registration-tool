8fd1cf1e19ad01e1f84af653353a48b0d90feaed
stevejj4 <jumaomondi16@gmail.com>
2025-10-23 13:19:47 +0300

    adding getters to fetch data and not null

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,37 @@
 package org.example;
 
+import java.sql.SQLWarning;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        registration register = new registration();
+
+        System.out.println("Enter your first name");
+        register.setFirstName(scanner.nextLine());
+
+        System.out.println("Enter your second name:");
+        register.setSecondName(scanner.nextLine());
+
+        System.out.println("Enter your date of birth (MM-DD-YYYY):");
+        register.setDateOfBirth(scanner.nextLine());
+
+        System.out.println("Enter your gender:");
+        register.setGender(scanner.nextLine());
+
+        System.out.println("Enter your phone number:");
+        register.setPhoneNumber(scanner.nextLine());
+
+        System.out.println("Enter alternative phone number:");
+        register.setAltPhoneNumber(scanner.nextLine());
+
+        register.displayInfo();
+
+        scanner.close();
     }
 }
--- src/main/java/org/example/registration.java
+++ src/main/java/org/example/registration.java
@@ -1,4 +1,66 @@
 package org.example;
 
 public class registration {
+    private String firstName;
+    private String secondName;
+    private String dateOfBirth;
+    private String gender;
+    private String idNumber;
+    private String phoneNumber;
+    private String altPhoneNumber;
+
+    //setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setSecondName(String secondName) {
+        this.secondName= secondName;
+    }
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setAltPhoneNumber(String altPhoneNumber) {
+        this.altPhoneNumber = altPhoneNumber;
+    }
+    // getters
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getSecondName() {
+        return secondName;
+    }
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getIdNumber() {
+        return idNumber;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public String getAltPhoneNumber() {
+        return altPhoneNumber;
+    }
+
+    public void displayInfo() {
+        System.out.println("Registration successful, confirm your details");
+        System.out.println("First name " + firstName);
+        System.out.println("Second name: " + secondName);
+        System.out.println("Date of birth (MM-DD-YYYY: " + dateOfBirth);
+        System.out.println("Gender:" + gender);
+        System.out.println("Phone number: " + phoneNumber);
+        System.out.println("Alternative phone number: " + altPhoneNumber);
+    }
 }
